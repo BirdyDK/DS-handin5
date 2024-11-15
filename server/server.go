d14dc24570ff1f5b3a0fedf2b8d066fd4877f0a7
Akn <adni@itu.dk>
2024-11-15 14:27:57 +0100

    flags added

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -17,6 +18,10 @@ type auctionServer struct {
 	pb.UnimplementedAuctionServer
 	highestBid  int32
 	auctionOver bool
+	isLeader    bool
+	nodeID      int32
+	leaderID    int32
+	peers       []pb.AuctionClient
 	mutex       sync.Mutex
 }
 
@@ -46,6 +51,46 @@ func (s *auctionServer) Result(ctx context.Context, req *pb.ResultRequest) (*pb.
 	return &pb.ResultResponse{HighestBid: s.highestBid, Winner: "auction is over, final result"}, nil
 }
 
+func (s *auctionServer) Election(ctx context.Context, req *pb.ElectionRequest) (*pb.ElectionResponse, error) {
+	if req.NodeId > s.nodeID {
+		s.mutex.Lock()
+		s.isLeader = false
+		s.mutex.Unlock()
+		go s.startElection()
+	}
+	return &pb.ElectionResponse{}, nil
+}
+
+func (s *auctionServer) Victory(ctx context.Context, req *pb.VictoryRequest) (*pb.VictoryResponse, error) {
+	s.mutex.Lock()
+	s.leaderID = req.LeaderId
+	s.isLeader = (s.leaderID == s.nodeID)
+	s.mutex.Unlock()
+	return &pb.VictoryResponse{}, nil
+}
+
+func (s *auctionServer) startElection() {
+	s.mutex.Lock()
+	s.isLeader = false
+	s.mutex.Unlock()
+
+	for _, peer := range s.peers {
+		_, err := peer.Election(context.Background(), &pb.ElectionRequest{NodeId: s.nodeID})
+		if err == nil {
+			return
+		}
+	}
+
+	s.mutex.Lock()
+	s.leaderID = s.nodeID
+	s.isLeader = true
+	s.mutex.Unlock()
+
+	for _, peer := range s.peers {
+		_, _ = peer.Victory(context.Background(), &pb.VictoryRequest{LeaderId: s.nodeID})
+	}
+}
+
 func (s *auctionServer) startAuction(duration time.Duration) {
 	time.Sleep(duration)
 	s.mutex.Lock()
@@ -54,7 +99,27 @@ func (s *auctionServer) startAuction(duration time.Duration) {
 }
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	var (
+		portID      = flag.Int("portID", 0, "Port ID")
+		baseport    = flag.Int("basePort", 0, "Base Port")
+		servercount = flag.Int("serverCount", 0, "Server Count")
+		isLeader    = flag.Bool("isLeader", false, "Is Leader")
+	)
+	flag.Parse()
+
+	if *portID == 0 {
+		log.Println("Port ID must be specified")
+	}
+
+	if *baseport == 0 {
+		log.Println("Base Port must be specified")
+	}
+
+	if *servercount == 0 {
+		log.Println("Server count must be specified")
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *portID))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -63,6 +128,27 @@ func main() {
 	server := &auctionServer{}
 	pb.RegisterAuctionServer(grpcServer, server)
 
+	server.isLeader = *isLeader
+	server.leaderID = int32(*baseport)
+
+	var peerAddrList []int
+	for i := *baseport; i < *servercount; i++ {
+		if i == *portID {
+			continue
+		}
+
+		peerAddrList = append(peerAddrList, i)
+	}
+
+	for _, addr := range peerAddrList {
+		conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", addr), grpc.WithInsecure())
+		if err == nil {
+			server.peers = append(server.peers, pb.NewAuctionClient(conn))
+		} else {
+			log.Printf("failed to connect to peer %s: %v", addr, err)
+		}
+	}
+
 	// Start the auction with a 2-minute timer
 	go server.startAuction(2 * time.Minute)
 
@@ -70,3 +156,5 @@ func main() {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
+
+// go run server.go --portid=? --baseport=? --servercount=? --isleader=true
